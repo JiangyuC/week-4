bc219fe061d9487dc4fb77e44d1c67ea138cecf1
JiangyuC <jc4322@columbia.edu>
2015-10-14 15:19:26 -0400

    Week 4 Assignment

--- app.py
+++ app.py
@@ -6,15 +6,37 @@ import time
 import sys
 
 import pyorient
+from Queue import Queue
 
 app = Flask(__name__)
+q=Queue()
 
+def event_stream():
+    while True:
+        result = q.get()
+        yield 'data: %s\n\n' % str(result)
+
+@app.route('/eventSource/')
+def sse_source():
+    return Response(
+            event_stream(),
+            mimetype='text/event-stream')
+            
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/getData/")
 def getData():
+	
+	q.put("starting data query...")
+
+	lat1 = str(request.args.get('lat1'))
+	lng1 = str(request.args.get('lng1'))
+	lat2 = str(request.args.get('lat2'))
+	lng2 = str(request.args.get('lng2'))
+
+	print "received coordinates: [" + lat1 + ", " + lat2 + "], [" + lng1 + ", " + lng2 + "]"
 	
 	client = pyorient.OrientDB("localhost", 2424)
 	session_id = client.connect("root", "password")
@@ -25,20 +47,19 @@ def getData():
 	if client.db_exists( db_name, pyorient.STORAGE_TYPE_MEMORY ):
 		client.db_open( db_name, db_username, db_password )
 		print db_name + " opened successfully"
+		q.put('Success')
 	else:
 		print "database [" + db_name + "] does not exist! session ending..."
 		sys.exit()
+		q.put('Fail')
 		
-	lat1 = 22.532498
-	lat2 = 22.552317
-
-	lng1 = 114.044329
-	lng2 = 114.076644
-
 	query = 'SELECT FROM Listing WHERE latitude BETWEEN {} AND {} AND longitude BETWEEN {} AND {}'
 
 	records = client.command(query.format(lat1, lat2, lng1, lng2))
 
+        random.shuffle(records)
+	records = records[:100]
+	
 	numListings = len(records)
 	print 'received ' + str(numListings) + ' records'
 
@@ -54,6 +75,8 @@ def getData():
 		feature["geometry"]["coordinates"] = [record.latitude, record.longitude]
 
 		output["features"].append(feature)
+		
+		q.put('idle')
 
 	return json.dumps(output)
 
